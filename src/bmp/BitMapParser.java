055c2af8da93f7f6672f6caa9caf7af096f3c43c
egfedoseev <eg.fedoseev2014@yandex.ru>
2024-03-19 23:33:49 +0300

    add expect functions

--- src/bmp/BitMapParser.java
+++ src/bmp/BitMapParser.java
@@ -1,27 +1,72 @@
 package bmp;
 
+import bmp.byteblock.FileHeader;
+import bmp.byteblock.Info;
+import bmp.byteblock.PixelTable;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 
 public class BitMapParser {
     private byte[] bytes;
     private int pos;
 
-    public BitMap parse(Path path) throws IOException {
-        bytes = Files.readAllBytes(path);
+    private IllegalArgumentException error(String message) {
+        throw new IllegalArgumentException("Error at position " + pos + ": " + message);
+    }
+
+    private void expect(byte[] expected) {
+        boolean ret = (pos + expected.length <= bytes.length);
+        for (int i = pos; i < pos + expected.length; ++i) {
+            if (expected[i - pos] != bytes[i]) {
+                ret = false;
+                break;
+            }
+        }
+        if (!ret) {
+            throw error("Expected: " + Arrays.toString(expected) +
+                    "\nFound: " + Arrays.toString(Arrays.copyOfRange(bytes, pos, pos + expected.length)));
+        }
+        pos += expected.length;
+    }
+
+    private void expect(byte expected) {
+        if (pos >= bytes.length || bytes[pos] != expected) {
+            String found = "";
+            if (pos < bytes.length) {
+                found = Byte.toString(bytes[pos]);
+            }
+            throw error("Expected: " + expected + "\nFound: " + found);
+        }
+    }
+
+    public BitMap parse(Path path) {
+        try {
+            bytes = Files.readAllBytes(path);
+        } catch (IOException e) {
+            System.err.println("Can't read file\nIOException: " + e.getMessage());
+            return null;
+        }
         pos = 0;
-        BitMap.FileHeader fileHeader = parseFileHeader();
-        BitMap.Info info = parseInfo();
 
-        return null;
+        FileHeader fileHeader = parseFileHeader();
+        Info info = parseInfo();
+        PixelTable pixels = parsePixelTable();
+
+        return new BitMap(fileHeader, info, pixels);
     }
 
-    private BitMap.FileHeader parseFileHeader() {
-        return null;
+    private FileHeader parseFileHeader() {
+
     }
 
-    private BitMap.Info parseInfo() {
-        return null;
+    private Info parseInfo() {
+
+    }
+
+    private PixelTable parsePixelTable() {
+
     }
 }
